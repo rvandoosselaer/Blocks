9cf80f5598e194afa609c2cfa8342f6fc3bad3d2
rvandoosselaer <remy.vand@gmail.com>
2020-01-03 10:11:56 +0100

    update readme list indents; Change wildcard import to specific imports.

--- src/test/java/com/rvandoosselaer/blocks/ChunkCacheTest.java
+++ src/test/java/com/rvandoosselaer/blocks/ChunkCacheTest.java
@@ -10,7 +10,11 @@ import org.junit.jupiter.api.Test;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * @author: rvandoosselaer
