402e7ecc181b10ecdbfe7835b839813d71a4200b
rvandoosselaer <remy.vand@gmail.com>
2020-05-27 10:35:56 +0200

    Prepare for release 1.5.1.

---
+++ docs/1.5.1/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.rvandoosselaer.blocks"},{"l":"com.rvandoosselaer.blocks.filters"},{"l":"com.rvandoosselaer.blocks.protobuf"}]
